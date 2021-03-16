589d9c691664fd42eb0e8eeb2bf1bb65ea165f79
thalif <thalif12@gmail.com>
2021-03-16 17:35:16 +0530

    Add project files.

---
+++ DeQue/Class1.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DeQue
+{
+    public class Deque
+    {
+        static int Capacity ;
+        int[] Data = new int[Capacity];
+
+        int Old;
+        int New;
+        int Count;
+
+        public bool IsEmpty
+        {
+            get
+            {
+                return Count == 0;
+            }
+        }
+
+        public void InsertLast(int item)
+        {
+            if(IsEmpty)
+            {
+                New = -1;
+                Old = -1;
+                Data[++New] = item;
+                Old++;
+                Count++;
+            }
+            else
+            {
+                if((New + 1) == Capacity && Count < Capacity)
+                {
+                    Data[New = 0] = item;
+                    Count++;
+                }
+                else if (Count < Capacity)
+                {
+                    Data[++New] = item;
+                    Count++;
+                }
+                else if(Count == Capacity)
+                {
+                    ReSize();
+                    InsertLast(item);
+                }
+            }
+        }
+        public void ReSize()
+        {
+            if(Count == Capacity)
+            {
+                int[] tempArray = new int[Capacity *= 2];
+
+                int tempHead = Old;
+                int tempTail = 0;
+
+                for(int i = 0; i < Count; i++)
+                {
+                    if(tempHead < Count)
+                    {
+                        tempArray[i] = Data[tempHead++];
+                    }
+                    else if(tempTail < Old)
+                    {
+                        tempArray[i] = Data[tempTail++];
+                    }
+                }
+                Data = tempArray;
+                Old = 0;
+                New = Count - 1;
+            }
+        }
+
+        public void RemoveLast()
+        {
+            if(!IsEmpty)
+            {
+                Data[New--] = 0;
+                Count--;
+            }
+            if(New < 0)
+            {
+                New = Capacity - 1;
+            }
+        }
+
+        public void RemoveFirst()
+        {
+            if (!IsEmpty)
+            {
+                Data[Old] = 0;
+                Old++;
+                Count--;
+            }
+        }
+
+        public Deque()
+        {
+            Capacity = 5;
+            Data = new int[Capacity];
+
+            Old = -1;
+            New = -1;
+            Count = 0;
+        }
+    }
+}
---
+++ TestDeQue/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DeQue;
+
+namespace TestDeQue
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Deque dq = new Deque();
+            dq.InsertLast(10);
+            dq.InsertLast(20);
+            dq.InsertLast(30);
+            dq.RemoveFirst();
+            dq.InsertLast(100);
+            Console.ReadKey();
+        }
+    }
+}
